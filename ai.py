6da3ba848c13863e779699e0ce0330990604b102
fabian <fabian.bark@gmail.com>
2023-08-18 16:31:46 +0200

    .

--- ai.py
+++ ai.py
@@ -1,5 +1,14 @@
 import numpy as np
 import random as rn
-pixels=28
-a1 = np.empty(pixels)
+pixels=28**2
+l1=pixels
+l2=16
+l3=16
+l4=10
+a1 = np.empty(l1)
+w1 = np.empty(l2,l1)
+wa1= w1*a1
+wa1s = np.empty(l2)
+for i in range(l2):
+    for j in range(l1):
 print(a1)
